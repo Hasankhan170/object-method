a71748dc9fe5ef4a015d71e5f56c77be0d1cf35a
Hasankhan170 <hasankhanali0316@gmail.com>
2024-05-06 03:33:24 -0700

    object

---
+++ app.js
@@ -0,0 +1,83 @@
+
+// Q : 1
+
+// var itemsArray = [
+
+//     {name:"juice", price:"50", quantity:"3"},
+    
+//     { name:"cookie", price:"30", quantity:"9"},
+    
+//     {name:"shirt", price:"880", quantity:"1"},
+    
+//     {name:"pen", price:"100", quantity:"2"}];
+
+
+// function totalPriceOfAllItems(){
+//     let totalPriceAllItems= 0;
+//     itemsArray.forEach(function(item){
+//         var totalItems = parseFloat(item.price)* parseFloat(item.quantity);
+//         console.log("the total price of" + item.name + " " + totalItems)
+//         totalPriceAllItems += totalItems;
+
+//     })
+//     console.log("Total price of all items: " + totalPriceAllItems);
+// }
+
+// totalPriceOfAllItems();
+
+// DONE 
+
+
+// Q : 2
+
+// const obj = {
+//     name: "hasan",
+//     email : "hasankhan@gmail.com",
+//     password : "hasan",
+//     age : 20,
+//     phone : 1234567890,
+//     city : "karachi",
+//     country: "pakistan"
+// }
+// if(obj.hasOwnProperty("age")) {
+//     console.log("age property exists in object");
+// }else{
+//     console.log("age property does not exists in object");
+// }
+
+// if(obj.hasOwnProperty("country")) {
+//     console.log("country property exists in object");
+// }else{
+//     console.log("country property does not exists in object");
+// }
+
+// if(obj.hasOwnProperty("firstName")) {
+//     console.log("firstName property exists in object");
+// }else{
+//     console.log("firstName property does not exists in object");
+// }
+
+// if(obj.hasOwnProperty("lastName")) {
+//     console.log("lastName property exists in object");
+// }else{
+//     console.log("lastName property does not exists in object");
+// }
+
+// DONE 
+
+
+// Q : 3
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
